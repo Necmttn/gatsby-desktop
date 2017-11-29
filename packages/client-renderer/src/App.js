cbe343786f4f8b594d71a8ba27a435e0965e68ff
Necmettin Karakaya <necmettin.karakaya@gmail.com>
2017-11-29 23:04:57 +0800

    testing graphql connection

--- packages/client-renderer/src/App.js
+++ packages/client-renderer/src/App.js
@@ -2,6 +2,38 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import gql from 'graphql-tag';
+import cors from 'cors'
+
+const whitelist = [
+  'http://localhost:3000'
+];
+const corsOpti
+
+const client = new ApolloClient({
+  link: new HttpLink({ uri: 'http://127.0.0.1:8000' }),
+  cache: new InMemoryCache()
+});
+
+
+client.query({
+  query: gql`
+    query TodoApp {
+      todos {
+        id
+        text
+        completed
+      }
+    }
+  `,
+})
+  .then(data => console.log(data))
+  .catch(error => console.error(error));
+
+
 class App extends Component {
   render() {
     return (
@@ -10,6 +42,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to Gatsby Desktop</h1>
         </header>
+        <div>
+          connect to gatsby process.
+        </div>
       </div>
     );
   }
